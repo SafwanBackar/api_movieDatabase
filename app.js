304175222b0394ba6bbab6f6f630dc94162d2cc0
SafwanBackar <sbackar29@gmail.com>
2022-03-08 10:49:37 +0530

    apiMovies

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+app.set('view engine', 'ejs');
+
+
+
+// app.get('/', function (req, res) {
+//     res.render('search');
+// })
+
+
+app.get('/results', function (req, res) {
+    // var query = req.query.search
+    request('http://www.omdbapi.com/?s=california&apikey=thewdb', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            res.render('results', { data: data })
+        }
+    });
+});
+
+
+app.listen(1222, function () {
+    console.log('Server has started')
+})
